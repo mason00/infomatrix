5c83ac24ab0b60d75263a2f754d1dc6e7bbcd5a8
mason00 <masondev@gmail.com>
2016-08-10 14:45:53 +1000

    initial commit

---
+++ Risk.Tests/BetRepositoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Risk.Data;
+using System.Linq;
+
+namespace Risk.Tests
+{
+    [TestClass]
+    public class BetRepositoryTests
+    {
+        [TestMethod]
+        public void LoadSettledTest()
+        {
+            var path = @"C:\Users\ZhenXin\Source\Repos\infomatrix\Risk.Tests\TestFiles\Settled.csv";
+            var repo = new BetRepository(path, null);
+            Assert.AreEqual(50, repo.SettledRecords.Count());
+        }
+    }
+}
---
+++ Risk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Risk.MvcApplication" Language="C#" %>
